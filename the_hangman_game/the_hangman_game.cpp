1e829640c973acfc6bce8334118d79bba3a8284c
JGochi <jagoda.goczal@gmail.com>
2024-12-09 17:56:01 +0100

    Updated Structure and new word
    new functions

--- the_hangman_game/the_hangman_game.cpp
+++ the_hangman_game/the_hangman_game.cpp
@@ -1,26 +1,78 @@
 #include <iostream>
-
-Initialize();
-GetInput();
-UpdateGame();
-Render();
-
-ShutDown();
+#include <fstream> //Praca z plikami
+#include <vector> //Wektor
+#include <string> //String
+#include <cstdlib> //Generacja losowych liczb
+#include <ctime> //Generacja losowych liczb
 
 using namespace std;
 
+//Zmienne globalne
+vector<string> wordPool; //S³owa z pliku txt
+string selectedWord;
+string displayedWord;
+
+int attemptsLaft = ;
+bool exitGame = false;
+bool gameWon = false;
+
+void Initialize();
+void LoadWordsFromFile();
+void DrawnWord();
+void GetInput()
+void UpdateGame();
+void Render();
+void ShutDown();
+
+
+
 int main()
 {
-	Initialize();
+	Initialize(); //Przygotowanie
 
 	while (!exitGame)
 	{
-		GetInput();
-		UpdateGame();
-		Render();
+		GetInput(); //Pobranie odpowiedzi od u¿ytkownika
+		Render(); //Bie¿¹cy stan
 	}
 
-	ShutDown();
+	ShutDown(); //Koniec
 
 	return 0;
+}
+
+void Initialize()
+{
+	LoadWordsFromFile();
+	DrawnWord();
+}
+
+void LoadWordsFromFile()
+{
+	//Za³adowanie s³ów z pliku hasla_do_gry.txt}
+}
+
+void DrawnWord()
+{
+	//Losowanie s³owa z puli
+}
+
+void GetInput()
+{
+	//Pobranie litery od u¿ytkownika
+}
+
+void UpdateGame(char guess)
+{
+	//Sprawdza, czy litera jest w selectedWord
+}
+
+void Render()
+{
+	//Wyœwietla bie¿¹cy stan
+}
+
+void ShutDown()
+{
+	//Komunikat o zakoñczeniu gry
 }
